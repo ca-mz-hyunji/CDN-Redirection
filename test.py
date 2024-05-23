8b9e8d485ee91d6a22573fafec92a884c97a9e0c
Hyunji Kim <hji.kim@megazone.com>
2024-05-22 23:35:00 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -5,71 +5,62 @@ import json
 from urllib.parse import urlparse
 import time
 
-def valid_url(url):
-    try:
-        check = urlparse(url)
-        return all([check.scheme, check.netloc])
-    except Exception:
-        return False
+# 1.3: After running the "grep" command, read the output log file names
+def LogFiles(grep):
+    result = subprocess.run(grep, stdout=subprocess.PIPE, text=True, shell=True)
+    log_files = result.stdout.splitlines()
+    return log_files
 
-def curl(url):
-    ip_list = {'www.kia.com':'34.160.76.133',
-               'www.hyundai.com':'34.107.246.212',
-               'www.hyundaiusa.com':'34.160.79.194',
-               'www.genesis.com':'34.149.135.209',
-               'www.casper.hyundai.com':'35.244.253.146',
-               'casper.hyundai.com':'34.64.165.126',
-               'sweb.hyundaiusa.com':'34.149.243.31',
-               'sweb-owners.genesis.com':'34.117.224.125',
-               'owners.genesis.com':'34.36.215.56',
-               'owners-kia.com':'34.110.185.95',
-               'preprod2-eu.kia.com':'34.102.172.183',
-               'connectstore.kia.com':'34.49.88.90'}
-    ip = ''
-
-    # Ex. 'url = https://www.kia.com/hello/'
-    url_parts = url.split("://")[1] # url_parts = ['https', 'www.kia.com/hello/'][1] == 'www.kia.com'
-    host = url_parts.split("/")[0] # host = ['www.kia.com', 'hello', ''][0] == 'www.kia.com'
-
-
-    # Assign IPs based on the documentation
-    if host in ip_list:
-        ip = ip_list[host]
-    else:
-        print(f"{url} is not a valid URL")
-        return -1
-
-    new_url = url.replace(host, ip, 1) # 1 - only the first occurance will be replaced
-
-    shellCommand = 'curl -I -H "HOST:{}"'.format(host) + ' ' + new_url
-    print(shellCommand) # 'curl -I -H "HOST:www.kia.com"' https://34.160.76.133/hello
-
-
+# 1.5: Open the log file, find the virtual host name between "id=- -" and "HTTP" (from the bottom)
 def HostName(log_file):
+    search = []
+    host_name = ''
     with open(log_file, 'r') as file:
-        # Read lines from the end of the file
-        lines = file.readlines()  # Reverse the list to read from the bottom
-        lines.reverse()
+        lines = file.readlines()    # return lists
         for line in lines:
-            if 'id=- -' in line:
-                return 0
-    return -1
+            id_index = line.find("id=- -")
+            http_index = line.find("HTTP")
+            if (id_index != -1) and (http_index != -1):
+                host_name = line[id_index+7:http_index-1]
+                search.append(host_name)
+            else:
+                None
+    file.close()
+    return search[-1]
+
+# 1.6: Find the configuration file in "setting.json".
+# Configuration file location is next to the basehost under the virtual hostname you found.
+def BaseHost(json_file):
+    '''
+    json_file = "/usr/local/m2/setting.json"
+    
+    if not os.path.exists(json_file):
+        print(f"JSON file '{json_file}' not found.")
+    '''
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+        
+    file.close()
 
 def main():
-    src = input("Type in the source URL:  ")
+    #src = input("Type in the source URL:  ")
     #dst = input("Type in the destination URL:  ")
 
-    url = valid_url(src)
+    #cmd = GrepCommand(src)
+    #print(cmd)
 
-    while url == False:
-        print(f"{url} is not a valid URL")
-        src = input("Type in the source URL:  ")
-    else:
-        curlCommend = curl(src)
+    '''
+    cmd = input("Type in the command:  ")
+    logLists = LogFiles(cmd)
+    for log in logLists:
+        print(log)
+        hostname = HostName(log)
+        HostNames.append(hostname)
+    hostname = HostName('example_log')
+    print(hostname)
+    '''
 
-    HostNames = []
-    
-    #file = HostName('example_log')
+    BaseHost('www.kia.com-acl.json')
 
     
 if __name__ == '__main__':
