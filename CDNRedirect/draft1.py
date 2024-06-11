63178eb12fa5835aedc80c19a5ad1cb36a6f1be6
Hyunji Kim <hji.kim@megazone.com>
2024-06-11 16:39:07 -0400

    Finished userInput function

--- CDNRedirect/draft1.py
+++ CDNRedirect/draft1.py
@@ -22,38 +22,32 @@ IP_dict = {
 def splitURL(url):
     # https:// (protocol) www. (subdomain) kia.com (domain name) /hello/world/ (path/page)
     try:
-        protocol = url.split("://")[0] # protocol = ['https', 'www.kia.com/hello/world/'][0] = 'https'
-        domain_path = url.split("://")[1] # domain_path = 'www.kia.com/hello/world/'
+        url = url.lower()
+        domain_path = url.split("://")[1] # domain_path = ['https', 'www.kia.com/hello/world/'][1] = 'www.kia.com/hello/world/'
         domain = domain_path.split("/")[0] # domain = ['www.kia.com', 'hello', 'world', ''][0] = 'www.kia.com'
-        return protocol, domain
+        path_from = domain_path.replace(domain, '')
+        return domain, path_from
     except Exception:
-        return None
+        return None, None
     
 # 1: Get inputs from User
 def userInput():
     src_url = input("Enter <SOURCE URL>: ")
-    protocol, domain = splitURL(src_url)
-    if domain not in IP_dict:
+    domain, path_from = splitURL(src_url)
+    while domain not in IP_dict:
         src_url = input("Enter <SOURCE URL>: ")
-        protocol, domain = splitURL(src_url)
+        domain, path_from = splitURL(src_url)
+    ip = IP_dict[domain]
 
+    dst_url = input("Enter <DESTINATION URL: ")
+    action = input("Enter <ACTION> <add/change/delete>: ")
+    while action.lower() not in ('add','change','delete'):
+        action = input("Enter <ACTION> <add/change/delete>: ")
 
-# hostIP + inputSrc + inputMode + destination URL -> input 한번에 받기
-# 6개 possible cases 따로
+    return domain, ip, path_from, dst_url, action
 
-# Non-csv file method
-# Phase 1
-# 1: Get inputs from User
-# 2: Send Curl (check Destination & make a log if there wasn't)
-# 3: Check the 6 cases (add, modify, delete) (destination = location ?: same, different)
-# 4: Grep Command --> Find Log Files (sort into shortest first)
-# 5: Open the (shortest) log file and find Virtual Hostname
-# 6: Find JSON configuration file for the Virtual Hostname (from /usr/local/m2/setting.json)
-# 7: Open the JSON config file and search for the redirect_from
-# Phase 2
-# 8: Check 2 cases (redirect_from exists? (modify, delete -> yes) (add -> no))
-# *: Copy a backup file in a subdirectory
+def main():
+    userInput()
 
-
-
-# csv file method (for multiple redirections)
+if __name__=='__main__':
+    main()
