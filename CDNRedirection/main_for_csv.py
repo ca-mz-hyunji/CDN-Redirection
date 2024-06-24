56f8d19bef8d85b2e9df9aee6f9e62640d870a61
Hyunji Kim <hji.kim@megazone.com>
2024-06-24 12:41:45 -0400

    tested importing the csv file done

---
+++ CDNRedirection/main_for_csv.py
@@ -0,0 +1,104 @@
+import os
+import subprocess
+import sys
+import json
+import re
+
+from modules.helpers import border_msg
+from modules.userInputcsv import userInputcsv
+'''
+from modules.curlCommand import curlCommand
+from modules.checkActionExcel import checkActionExcel
+from modules.grepCommand import grepCommand
+from modules.findVirtualHostname import findVirtualHostname
+from modules.findBaseHost import findBaseHost
+from modules.openConfigFile import openConfigFile
+from modules.userConfirm import userConfirm
+from modules.createBackup import createBackup
+from modules.updateFile import updateFile
+from modules.helpers import userData, border_msg
+'''
+
+
+def main():
+    ### Phase 1 ###
+    check_action_failed = {}
+    check_config_failed = {}
+    user_confirm_failed = {}
+
+    # Step 1: Get inputs from User
+    #excel_file_path = input("Type in the Excel file path:  ")
+    excel_file_path = "C:\\Users\\Kim\\Desktop\\GitHub\\Redirecting-Automation\\CDNRedirection\\testing_files\\kia_test.csv"
+
+    if not os.path.exists(excel_file_path):
+        border_msg(f"CSV file '{excel_file_path}' not found.")
+        return
+        
+    user_input_dict, domain_failed = userInputcsv(excel_file_path)
+    # user_input_dict = {{'src_url', 'domain', 'ip', 'path_from', 'dst_url', 'action'}}
+    '''
+    for user_input in user_input_dict:
+        # Step 2: Send Curl (check Destination & make a log if there wasn't)
+        # print(f'{user_input+1}: {user_input_dict[user_input]}')
+        src_url, domain, ip, path_from, dst_url, action = userData(user_input_dict[user_input])
+
+        loc_eq_dst, location = curlCommand(domain, ip, path_from, dst_url, "Excel")
+    
+        # Step 3: Check the 8 cases (Add, Modify, Delete)
+        new_action = checkActionExcel(src_url, loc_eq_dst, location, action)
+
+        if action != new_action:    # if new_action is different from the initial action or False
+            if new_action == False:
+                check_action_failed[user_input] = user_input_dict[user_input]
+                continue
+            else:
+                user_input_dict[user_input]['action'] = new_action
+
+    for failed_index in check_action_failed:
+        print(f"\nERROR: Index {failed_index+1} failed to check action. Try it again later: \nsrc_url: {check_action_failed[failed_index]['src_url']} & dst_url: {check_action_failed[failed_index]['dst_url']}")
+        user_input_dict.pop(failed_index)
+        
+    print(user_input_dict)
+
+    return 0
+    for user_input in user_input_dict:
+        # Step 4: Grep Command --> Find Log Files (sort into shortest first)
+        log_files = grepCommand(path_from)
+            
+        # Step 5: Open the (shortest) log file (don't use /origin.log) and find Virtual Hostname for Path_from
+        virt_hosts = findVirtualHostname(log_files, path_from)
+            
+        # Step 6: Find BaseHost for the Virtual Hostname (from /usr/local/m2/setting.json)
+        base_hosts = findBaseHost(virt_hosts)   # dictionary
+            
+        # Step 7: Open the JSON config file (located at BaseHost) and search for redirection rule (using Path_from)
+        config_pattern, config_location, config_file_loc, config_checked = openConfigFile(base_hosts, path_from, src_url, dst_url, action)
+
+        if config_checked == False:
+            continue
+
+        ### Phase 2 ###
+        # Step 8: Show redirection rule (pattern) to Add/Modify/Delete and Check user's confirmation for updating the config file
+        confirmed, rule = userConfirm(config_pattern, config_location, action, path_from, dst_url)
+
+        if confirmed == 'no':
+            continue
+
+        # Step 9: Copy a backup file in a subdirectory
+        createBackup(config_file_loc)
+
+        # Step 10: Update the Config File with redirection rule
+        updateFile(path_from, dst_url, action, config_file_loc)
+
+
+        # Step 11: Deploy
+
+
+        # Step 12: Test the redirection change by using Curl (every 2 minutes, 5 times max)
+    '''
+
+
+if __name__=='__main__':
+    main()
+    # C:\\Users\\Kim\\Desktop\\GitHub\\Automation\\CDNRedirection\\testing_files\\kia_test.xlsx
+    # /home/hji_kim/automation/kia_test.csv
---
+++ CDNRedirection/modules/userInputcsv.py
@@ -0,0 +1,62 @@
+import csv
+from .helpers import splitURL, IP_dict, border_msg
+
+def userInputcsv(excel_file_path):
+    user_input_dict = {}
+    domain_failed = {}
+
+    src_urls = []
+    dst_urls = []
+    
+    with open(excel_file_path, mode='r') as file:
+        lines = csv.reader(file)
+        # skip header
+        next(lines)
+    
+        # Assumme the first row is the header
+        # Assume the first (A) column is the source URL
+        # Assume the second (B) column is the Redirection code
+        # Assume the third (C) column is the destination URL
+
+        for line in lines:
+            src_urls.append(line[0])
+            dst_urls.append(line[2])
+
+    index = 0
+    while index < len(src_urls):
+        domain, path_from = splitURL(src_urls[index])
+        if domain not in IP_dict:
+            domain = None
+            ip = None
+        else:
+            ip = IP_dict[domain]
+        
+        user_input_dict[index] = {'src_url': src_urls[index],
+                                    'domain': domain,
+                                    'ip': ip,
+                                    'path_from': path_from,
+                                    'dst_url': dst_urls[index],
+                                    'action': 'add'}
+        index += 1
+
+    count = 0
+    for user_input in user_input_dict:
+        if user_input_dict[user_input]['domain'] == None:
+            domain_failed[count] = user_input_dict[user_input]
+        count += 1
+            
+    for failed_index in domain_failed:
+        border_msg(f"Index {failed_index+1}: {domain_failed[failed_index]['src_url']} does not have a valid domain name. Try it again later.")
+        user_input_dict.pop(failed_index)
+    print("")
+
+    ### For testing purpose - delete later
+    for item in user_input_dict:
+        print(f"[{item+1}]: {user_input_dict[item]}\n")
+
+    return user_input_dict, domain_failed
+
+'''
+if __name__=='__main__':
+    userInputcsv("C:\\Users\\Kim\\Desktop\\GitHub\\Redirecting-Automation\\CDNRedirection\\testing_files\\kia_test.csv")
+'''
